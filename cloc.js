6cbed282e783f45baa7dca55e0b0e942c6a2d1eb
alekseyivanov90 <54991556+alekseyivanov90@users.noreply.github.com>
2023-05-09 18:58:45 +0400

    Create cloc.js

---
+++ cloc.js
@@ -0,0 +1,13 @@
+switch(0) {
+    case gor.search("Лучегорск"):  // if (x === 'value1')
+      alert("Ok");
+      break
+  
+    case 'value2':  // if (x === 'value2')
+      // sds
+      break
+  
+    default:
+      // asda
+      break
+  }
