225d3bf5437fe16cefb29435cb8c2fb19bca7754
alekseyivanov90 <54991556+alekseyivanov90@users.noreply.github.com>
2023-05-09 19:09:42 +0400

    Update cloc.js

--- cloc.js
+++ cloc.js
@@ -1,6 +1,14 @@
+let rop;
+
+if(Tm.getMinutes() < 10){
+	rop = " 0"+ Tm.getMinutes();
+}else{
+	rop = Tm.getMinutes();
+}
+
 switch(0) {
     case document.getElementsByClassName("look_for")[0].innerText.search("Лучегорск"):  // if (x === 'value1')
-      alert("Ok");
+      document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (Tm.getHours() - 1) + ":" + rop;
       break
   
     case 'value2':  // if (x === 'value2')
