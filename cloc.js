bfb0de4b15a6b8ab07bf35fd7f2ba200f8fe9095
alekseyivanov90 <54991556+alekseyivanov90@users.noreply.github.com>
2023-05-11 16:47:28 +0400

    Update cloc.js

--- cloc.js
+++ cloc.js
@@ -258,7 +258,15 @@ switch(0) {
         }
         document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (jamm) + '\:' + rop;
         break
-
+		
+    case document.getElementsByClassName("look_for")[0].innerText.search("Калинин"):
+        jamm = Tm.getHours() - 1
+        if(jamm > 24){
+            jamm = jamm - 24
+        }
+        document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (jamm) + '\:' + rop;
+        break
+		
       default:
       // asda
       break
