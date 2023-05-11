0db5f0b673441f6adc4a0d3e45b0d64eebca5dc7
alekseyivanov90 <54991556+alekseyivanov90@users.noreply.github.com>
2023-05-11 18:28:35 +0400

    Update cloc.js

--- cloc.js
+++ cloc.js
@@ -323,6 +323,30 @@ switch(0) {
         document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (jamm) + '\:' + rop;
         break
 		
+    case document.getElementsByClassName("look_for")[0].innerText.search("Химки"):
+        jamm = Tm.getHours() - 1
+        if(jamm > 24){
+            jamm = jamm - 24
+        }
+        document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (jamm) + '\:' + rop;
+        break
+		
+    case document.getElementsByClassName("look_for")[0].innerText.search("Сургут"):
+        jamm = Tm.getHours() + 1
+        if(jamm > 24){
+            jamm = jamm - 24
+        }
+        document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (jamm) + '\:' + rop;
+        break
+
+    case document.getElementsByClassName("look_for")[0].innerText.search("Строитель"):
+        jamm = Tm.getHours() - 1
+        if(jamm > 24){
+            jamm = jamm - 24
+        }
+        document.getElementsByClassName("look_for")[0].innerText = document.getElementsByClassName("look_for")[0].innerText + " " + (jamm) + '\:' + rop;
+        break
+		
       default:
       // asda
       break
